4268ff0007535fabf9e7830cc478a83414493927
dmytro.iakovliev <dmytro.iakovliev@dev.zodiac.tv>
2020-06-18 11:02:17 +0300

    Add cli options support

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <optional>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -8,6 +9,7 @@
 #include <cstring>
 #include <fcntl.h>
 #include <byteswap.h>
+#include <getopt.h>
 
 #include "FD.h"
 
@@ -190,7 +192,7 @@ public:
         return soname;
     }
 
-    std::vector<const char*> needed_list(const std::map<const char*, const char*>& replacements) {
+    std::vector<const char*> needed_list(const std::map<std::string, std::string>& replacements) {
         std::vector<const char*> result;
 
         auto dynamic    = reinterpret_cast<typename Traits::Dyn*>(section_data(".dynamic"));
@@ -206,12 +208,12 @@ public:
                 char *needed_str = dynstr + rdi(dyn->d_un.d_val);
 
                 std::for_each(replacements.begin(), replacements.end(), [needed_str](auto& it) {
-                    if (::strcmp(it.first, needed_str) != 0)
+                    if (::strcmp(it.first.c_str(), needed_str) != 0)
                         return;
 
                     std::cout << "new needed +: " << it.second << std::endl;
                     size_t old_needed_size = ::strlen(needed_str);
-                    ::strncpy(needed_str, it.second, old_needed_size);
+                    ::strncpy(needed_str, it.second.c_str(), old_needed_size);
                     std::cout << "needed -: " << needed_str << std::endl;
                 });
 
@@ -240,28 +242,8 @@ protected:
         for (int i = 0; i < rdi(ehdr_->e_phnum); ++i)
             phdrs_.push_back(&((typename Traits::Phdr*)(content_ + rdi(ehdr_->e_phoff)))[i]);
 
-        std::cout << "segments headers: " << phdrs_.size() << std::endl;
-
         for (int i = 0; i < rdi(ehdr_->e_shnum); ++i)
             shdrs_.push_back(&((typename Traits::Shdr*)(content_ + rdi(ehdr_->e_shoff)))[i]);
-
-        std::cout << "sections headers: " << shdrs_.size() << std::endl;
-        std::for_each(shdrs_.begin(), shdrs_.end(), [this](auto* shdr) {
-            std::cout << "\t"  << shdr << ": " << section_name(shdr) << std::endl;
-        });
-
-        auto strtab = find_section(".strtab");
-        if (strtab)
-            std::cout << " .strtab: " << strtab << std::endl;
-
-
-        //auto needed = needed_list();
-        //std::cout << "needed:" << std::endl;
-        //std::for_each(needed.begin(), needed.end(), [](const auto *n) {
-        //    std::cout << "\t" << n << std::endl;
-        //});
-
-        //std::cout << "soname: " << soname() << std::endl;
     }
 
 private:
@@ -270,26 +252,6 @@ private:
     std::vector<typename Traits::Phdr*> phdrs_;
     std::vector<typename Traits::Shdr*> shdrs_;
 };
-
-template<ElfClass Class>
-int class_entry(void* content, Endian elf_endian) {
-    std::map<const char*, const char*> replacements;
-    replacements.insert(std::make_pair("libdiskmgr.so", "libdxxxxxx.so"));
-                                                      //"libdiskmgr.so"
-    if (elf_endian == Little) {
-        Elf<Class, Little> elf(content);
-                   //"libstorage.so"
-        elf.soname("libnsoname.so");
-        elf.needed_list(replacements);
-        return 0;
-    } else if (elf_endian == Big) {
-        Elf<Class, Big> elf(content);
-        elf.soname("libnsoname.so");
-        elf.needed_list(replacements);
-        return 0;
-    }
-    return -1;
-}
 
 std::pair<ElfClass, Endian> elf_class(caddr_t contents) {
     if (::memcmp(contents, ELFMAG, SELFMAG) != 0)
@@ -309,23 +271,152 @@ std::pair<ElfClass, Endian> elf_class(caddr_t contents) {
     return std::make_pair(None, Unknown);
 }
 
+struct Args {
+    const char* filename = nullptr;
+    const char* soname = nullptr;
+    std::map<std::string, std::string> neededs;
+
+    static std::optional<std::pair<std::string, std::string> > parse_needed(const char* n) {
+        std::string s(n);
+        auto it = s.find(',');
+        if (it == std::string::npos)
+           return std::nullopt;
+
+        std::string old_needed(s.c_str(), it);
+        std::string new_needed(s.c_str() + it + 1, s.size() - it);
+
+        if (old_needed.empty() || new_needed.empty())
+           return std::nullopt;
+
+        return std::make_pair(old_needed, new_needed);
+    }
+
+    void print(std::ostream& out = std::cout) const {
+        out << "Arguments:" << std::endl;
+        if (filename)
+            out << "\tinput file: " << filename << std::endl;
+        if (soname)
+            out << "\tnew soname: " << soname << std::endl;
+        std::for_each(neededs.begin(), neededs.end(), [&](auto& n) {
+            out << "\tnew needed: " << n.first << " -> " << n.second << std::endl;
+        });
+    }
+
+    static void show_usage(const char *program_name, std::ostream& out = std::cerr) {
+        out << "Usage: " << program_name << " <options>"                                  << std::endl;
+        out << "Were options are:"                                                        << std::endl;
+        out << "\t-f,--filename: File to patch."                                          << std::endl;
+        out << "\t-s,--soname  : New ELF soname."                                         << std::endl;
+        out << "\t-n,--needed  : New ELF needed in format: <old needed>,<new needed>."    << std::endl;
+        out << "\t-h,-?        : Show this help message."                                 << std::endl;
+    }
+
+    static std::optional<Args> parse_args(int argc, char** argv) {
+        Args args;
+
+        static const char *opt_string = "f:s:n:h?";
+
+        static const struct option long_opts[] = {
+            { "filename",   required_argument,  NULL, 'f' },
+            { "soname",     required_argument,  NULL, 's' },
+            { "needed",     required_argument,  NULL, 'n' },
+            { NULL,         no_argument,        NULL, 0 }
+        };
+
+        int long_index = 0;
+
+        int opt = getopt_long(argc, argv, opt_string, long_opts, &long_index);
+        while( opt != -1 ) {
+            switch(opt) {
+            case 'f':
+                args.filename = optarg;
+            break;
+            case 's':
+                args.soname = optarg;
+            break;
+            case 'n':
+            {
+                auto n = parse_needed(optarg);
+                if (!n) {
+                    std::cerr << "error: Wrong needed replacement option: " << optarg << std::endl;
+                    return std::nullopt;
+                }
+                args.neededs.insert(*n);
+            }
+            break;
+            case 0:
+                {
+                    switch(long_index) {
+                    case 0:
+                        args.filename = optarg;
+                    break;
+                    case 1:
+                        args.soname = optarg;
+                    break;
+                    case 2:
+                    case 'n':
+                    {
+                        auto n = parse_needed(optarg);
+                        if (!n) {
+                            std::cerr << "error: Wrong needed replacement option: " << optarg << std::endl;
+                            return std::nullopt;
+                        }
+                        args.neededs.insert(*n);
+                    }
+                    break;
+                    default:
+                        show_usage(argv[0]);
+                        return std::nullopt;
+                    }
+                }
+                break;
+            case 'h':
+            case '?':
+            default:
+                show_usage(argv[0]);
+                return std::nullopt;
+            };
+
+            opt = getopt_long(argc, argv, opt_string, long_opts, &long_index);
+        }
+
+        return args;
+    }
+};
+
+template<ElfClass Class>
+int class_entry(void* content, Endian elf_endian, const Args& args) {
+    if (elf_endian == Little) {
+        Elf<Class, Little> elf(content);
+        elf.soname(args.soname);
+        elf.needed_list(args.neededs);
+        return 0;
+    } else if (elf_endian == Big) {
+        Elf<Class, Big> elf(content);
+        elf.soname(args.soname);
+        elf.needed_list(args.neededs);
+        return 0;
+    }
+    return -1;
+}
+
 int main(int argc, char** argv) {
 
-    if (argc < 2) {
-        std::cerr << "No input file specified!" << std::endl;
+    auto args = Args::parse_args(argc, argv);
+    if (!args) {
         return -1;
     }
 
-    const char *source_file = argv[1];
+    args->print();
+    return 0;
 
-    FD fd(::open(source_file, O_RDWR));
+    FD fd(::open(args->filename, O_RDWR));
     if (fd.bad()) {
-        std::cerr << "Can't open " << source_file << "!" << std::endl;
+        std::cerr << "Can't open " << args->filename << "!" << std::endl;
         return -1;
     }
 
     std::cout << "    host: " << GetHostEndian::name << std::endl;
-    std::cout << "filename: " << source_file << std::endl;
     std::cout << "    size: " << fd.size()   << std::endl;
 
     caddr_t content = reinterpret_cast<caddr_t>(fd.mmap(0, 0, PROT_READ|PROT_WRITE));
@@ -335,16 +426,16 @@ int main(int argc, char** argv) {
     switch(el_class.first) {
     case Elf32:
     {
-        return class_entry<Elf32>(content, el_class.second);
+        return class_entry<Elf32>(content, el_class.second, *args);
     }
     break;
     case Elf64:
     {
-        return class_entry<Elf64>(content, el_class.second);
+        return class_entry<Elf64>(content, el_class.second, *args);
     }
     break;
     default:
-        std::cerr << source_file << ": not an ELF file!";
+        std::cerr << args->filename << ": not an ELF file!";
         return -1;
     };
 }
